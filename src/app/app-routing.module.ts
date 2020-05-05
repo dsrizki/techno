a19b418311eb62ba39956729551bd79f755072a3
Rizki%20DS <dsrizki0@gmail.com>
2020-05-05 20:56:31 +0700

    frontend aplikasi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'pembayaran',
     loadChildren: () => import('./pembayaran/pembayaran.module').then( m => m.PembayaranPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
